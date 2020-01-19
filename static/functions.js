d801357b158ff4b45076f0c05981d0dcfc584595
Ella Li <ellali@Ellas-MacBook-Air.local>
2020-01-19 00:44:06 -0500

    not working stuff

--- static/functions.js
+++ static/functions.js
@@ -1,3 +1,3 @@
-$('.bahen').click(function(){
+$('bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden bahen').on("click",function(){
     console.log("There was a click!");
   })
